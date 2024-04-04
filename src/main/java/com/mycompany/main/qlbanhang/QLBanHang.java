1e0822b8798bf599c2a291eb3aef8f705c428666
ThanhPhatFPT <156346544+ThanhPhatFPT@users.noreply.github.com>
2024-04-05 00:09:22 +0700

    how to fix?

---
+++ src/main/java/com/mycompany/main/qlbanhang/QLBanHang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main.qlbanhang;
+
+/**
+ *
+ * @author votha
+ */
+public class QLBanHang {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
