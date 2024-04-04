7870d2c5dc81047eff6e8b4d6d193adf0d2246f9
ThanhPhatFPT <156346544+ThanhPhatFPT@users.noreply.github.com>
2024-04-05 00:17:36 +0700

    how to fix?

---
+++ src/main/java/com/mycompany/main/qlbanhang/login.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main.qlbanhang;
+
+/**
+ *
+ * @author votha
+ */
+public class login {
+    private String taiKhoan;
+    private String matKhau;
+
+    public login() {
+    }
+
+    public login(String taiKhoan, String matKhau) {
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+    }
+
+    public String getTaiKhoan() {
+        return taiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        this.taiKhoan = taiKhoan;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+    
+    
+}
